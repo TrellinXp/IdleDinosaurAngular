652dd63466f6835674ab85d147aa41dc986b26dc
Peter Mayer <Peter_My@gmx.de>
2025-04-08 11:21:38 +0200

    Credits

--- IdleDinosaur/src/app/app.routes.ts
+++ IdleDinosaur/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HatchComponent } from './hatch/hatch.component';
 import { DinosaursComponent } from './dinosaurs/dinosaurs.component';
+import { CreditsComponent } from './credits/credits.component';
 
 export const routes: Routes = [
     { path: 'hatch', component: HatchComponent },
     { path: 'dinosaurs', component: DinosaursComponent },
+    { path: 'credits', component: CreditsComponent },
     { path: '',   redirectTo: '/dinosaurs', pathMatch: 'full' }
   ];
